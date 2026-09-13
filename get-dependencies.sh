d34a7b79d1dbcdfd75d12564dbdc47a5ee96da98
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-09-13 00:20:03 -0400

    debug

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -24,7 +24,7 @@ pacman -Syu --noconfirm \
 	xcb-util-cursor     \
 	xcb-util-keysyms    \
 	xcb-util-wm         \
-	zsync
+	zsync gtk3 gt6-base vulkan-radeon ffmpeg 
 
 if [ "$ARCH" = 'x86_64' ]; then
 		pacman -Syu --noconfirm libva-intel-driver
