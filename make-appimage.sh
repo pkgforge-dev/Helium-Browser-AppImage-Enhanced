6d84a9985d392e12e76ffe1d9505b54445a01ec8
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-09-13 00:55:28 -0400

    kek

--- make-appimage.sh
+++ make-appimage.sh
@@ -32,7 +32,7 @@ quick-sharun \
 	/usr/lib/libcloudproviders*   \
 	/usr/lib/libgtk-3.so*
 
-rm -rf ./AppDir/lib/sharun-preload
+rm ./AppDir/lib/sharun-preload/cross-libc-dlopen.so
 # Additional changes can be done in between here
 
 # Turn AppDir into AppImage
